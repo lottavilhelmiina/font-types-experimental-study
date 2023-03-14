bd9d670b73940aa74ab870d72c0863d32e69265f
LOTTA <lottiss@me.com>
2023-03-14 22:23:33 +0200

    Made a simple application for experimental study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Typography, Button } from '@mui/material'
+import useEventListener from '@use-it/event-listener'
+
+const ENTER_KEY = ['13', 'Enter'];
 
 function App() {
+  const [textTable, setTextTable] = useState([
+    "1, It is a wonderful day.",
+    "2, It is a disastrous day.",
+    "3, It is an okay day."
+  ]);
+  const [fontTable, setFontTable] = useState([
+    "Roboto",
+    "Helvetica",
+    "Segoe UI"
+  ]);
+  
+  const [tableID, setTableID] = useState(0)
+  const [ fontID, setFontID ] = useState(0)
+  const [isVisible, setIsVisible] = useState(true);
+  const [isIntro, setIsIntro] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+
+  function handler({ key }) {
+    if (ENTER_KEY.includes(String(key))) {
+      if (isIntro === true) {
+          setIsIntro(false);
+          const timer = setTimeout(() => {
+            setIsVisible(false);
+            console.log(tableID);
+            console.log(fontID);
+            
+          }, 5000);
+      
+          return () => clearTimeout(timer);
+      }
+      else {
+            // Update state with the input value
+    setTableID(tableID + 1);
+    setFontID(fontID + 1);
+    setIsVisible(true);
+    const secondTimer = setTimeout(() => {
+      setIsVisible(false);
+    }, 5000);
+
+    if (tableID > 1) {
+      setIsEnd(true);
+    }
+
+    return () => clearTimeout(secondTimer);
+      }
+    }
+  }
+  useEventListener('keydown', handler);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isIntro ? (
+      <Typography>
+          When you are ready to start the experiment, press Enter-key.
+        </Typography>
+        ) : (
+          isVisible && <Typography sx={{fontFamily: fontTable[fontID]}}>
+          {textTable[tableID]}
+        </Typography>
+          )}
+          {isEnd && <Typography>Experiment ended. Thank you for participating.</Typography>}
     </div>
   );
-}
+      }
 
 export default App;
