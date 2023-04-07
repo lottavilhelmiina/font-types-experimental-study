bd10c38d739a8ebd6acddd76690a2716f16dc677
LOTTA <lottiss@me.com>
2023-04-07 16:25:11 +0300

    Added a webworker that is responsible for the timer calculations

---
+++ src/worker-script.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-restricted-globals */
+
+const workercode = () => {
+  let timerInterval;
+  let time = 0;
+  self.onmessage = function ({ data: { turn } }) {
+    if (turn === "off" || timerInterval) {
+      clearInterval(timerInterval);
+      time = 600;
+    }
+    if (turn === "on") {
+      time = 600;
+      timerInterval = setInterval(() => {
+        time -= 1;
+        self.postMessage({ time });
+
+        if (time <= 0) {
+          clearInterval(timerInterval);
+          return;
+        }
+  
+      }, 1000);
+    }
+  };
+};
+
+let code = workercode.toString();
+code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
+
+const blob = new Blob([code], { type: "application/javascript" });
+const worker_script = URL.createObjectURL(blob);
+
+module.exports = worker_script;
